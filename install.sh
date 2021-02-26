c6d9d2f35276fb89be079dd462ddad16108aef7e
anonymousskt <77140403+anonymousskt@users.noreply.github.com>
2021-02-26 01:32:30 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+pkg upgrade
+cd
+git clone https://github.com/anonymousskt/BOTKIBADO
+clear
+cd ./BOTKIBADO
+bash install.sh
+cd
+cd ./BOTKIBADO
+npm start
+
+echo "[*] obrigado por utilizar o bot \"sadzin\" rauny agradece"
