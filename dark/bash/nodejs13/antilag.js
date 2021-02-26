0a377b1f37866527d76a2015551477f655d401dd
anonymousskt <77140403+anonymousskt@users.noreply.github.com>
2021-02-25 22:51:35 -0300

    Add files via upload

---
+++ dark/bash/nodejs13/antilag.js
@@ -0,0 +1,14 @@
+const antilag = (prefix) => { 
+	return `
+	
+	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Sadzin Bot*
+	
+[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do Youtube : Sadzinmaker*
+
+⧿⧽ *Não se esqueça de se inscrever*
+⧿⧽ *Ative os sinos*
+⧿⧽ *Atenciosamente Rauny*
+
+`
+}
+exports.antilag = antilag
